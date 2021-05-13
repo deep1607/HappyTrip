8ff466308c6f58b6b37c384ed705b7265b242c00
Deep <1607deep@gmail.com>
2021-05-13 14:49:58 +0530

    TEAMB-34 Admin can view the all city information.

--- src/main/java/com/myapp/spring/web/api/CityApi.java
+++ src/main/java/com/myapp/spring/web/api/CityApi.java
@@ -2,12 +2,14 @@ package com.myapp.spring.web.api;
 
 import java.util.List;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -35,13 +37,35 @@ public class CityApi {
 	@PostMapping("/bulk")
 	public ResponseEntity<List<City>> bulkProuctsInsert(@RequestBody List<City> city){
 	
+		
+	
 	return new ResponseEntity<List<City>>(repository.saveAll(city),HttpStatus.CREATED);
 	}
 	
 	
-
+	//Admin Search all cities displayed 
+	//http://localhost:8888/admin/cities
+	@GetMapping
+	public ResponseEntity<List<City>> viewall(){
+		return new ResponseEntity<List<City>>(repository.viewcity(),HttpStatus.OK);	
+	}
 	
-
+	
+	//Admin can find by city id
+	//http://localhost:8888/admin/cities/findCity/id:{id}
+	@GetMapping("findCity/id:{id}")
+	public ResponseEntity<City> findByCityId(@PathVariable("id") String id){
+		return new ResponseEntity<City>(repository.findBycityId(id).get(),HttpStatus.OK);
+	}
 	
 	
+	//Admin can find by city name
+	//http://localhost:8888/admin/cities/findCity/city:{city}
+	@GetMapping("findCity/city:{name}")
+	public ResponseEntity<City> findByCityName(@PathVariable("name") String name){
+		return new ResponseEntity<City>(repository.findBycityName(name).get(),HttpStatus.OK);
+	}
+	
+	
+		
 }
