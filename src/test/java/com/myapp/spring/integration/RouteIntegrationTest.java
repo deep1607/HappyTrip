4501347e17e991d3667a72cf841fd219b4ace94a
Deep <1607deep@gmail.com>
2021-05-16 19:46:01 +0530

    TEAMB-40 Admin can update route tested with integration testing

--- src/test/java/com/myapp/spring/integration/RouteIntegrationTest.java
+++ src/test/java/com/myapp/spring/integration/RouteIntegrationTest.java
@@ -1,10 +1,14 @@
-package com.myapp.spring.web.api;
+package com.myapp.spring.integration;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.mockito.Mockito.doReturn;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
@@ -13,7 +17,8 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 
-
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentMatchers;
@@ -27,6 +32,8 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultMatcher;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import com.fasterxml.jackson.core.io.JsonEOFException;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.myapp.spring.model.RouteModel;
 import com.myapp.spring.repository.RouteRepository;
@@ -35,28 +42,39 @@ import com.myapp.spring.service.RouteService;
 @SpringBootTest
 @AutoConfigureMockMvc
 
-public class RouteApiTest {
+public class RouteIntegrationTest {
 
-	@MockBean
+	@Autowired
 	private RouteRepository repository;
 	
-	@MockBean
+	@Autowired
 	private RouteService service;
 	
 	@Autowired
 	private MockMvc mockMvc;
 	
+	private static File DATA_JSON = Paths.get("src","test","resources","route.json").toFile();
+	
+	@BeforeEach
+	public void setUp() throws JsonEOFException,JsonMappingException,IOException{
+		RouteModel cities[]= new ObjectMapper().readValue(DATA_JSON, RouteModel[].class);
+		Arrays.stream(cities).forEach(repository::save);
+	}
 
+	@AfterEach
+	public void cleanUp() {
+		repository.deleteAll();
+		
+	}
+	
 	@Test
 	@DisplayName("Test Add route")
 	public void testAddNewRoute() throws Exception{
 		
 		RouteModel newroute=new RouteModel(4, "Mumbai", "Bangalore", 836);
-		RouteModel mockroute=new RouteModel(4, "Mumbai", "Bangalore", 836);
 		
-		String json = new ObjectMapper().writeValueAsString(mockroute);
+		String json = new ObjectMapper().writeValueAsString(newroute);
 		
-		doReturn(mockroute).when(repository).save(ArgumentMatchers.any());
 		
 		mockMvc.perform(post("/admin/route")
 				// Validate Status should be 200 ok and json response recived
@@ -71,20 +89,21 @@ public class RouteApiTest {
 	}
 	
 	@Test
-	@DisplayName("Test view all route information")
+	@DisplayName("Test view all city information")
 	public void testGetAllRoute() throws Exception {
 		
-		//Prepare Mock Product
-		RouteModel route1=new RouteModel(4, "Mumbai", "Bangalore", 836);
-		RouteModel route2=new RouteModel(5, "Delhi", "Bangalore", 1737);
-	
+		
+		RouteModel route1=new RouteModel(1, "Mumbai", "Delhi", 1163);
+		RouteModel route2=new RouteModel(2, "Delhi", "Mumbai", 1163);
+		RouteModel route3=new RouteModel(3, "Banglore", "Mumbai", 836);
+		
 		List<RouteModel> routes = new ArrayList<>();
 		routes.add(route1);
 		routes.add(route2);
+		routes.add(route3);
 		
 		String json = new ObjectMapper().writeValueAsString(routes);
 		
-		doReturn(routes).when(repository).viewAllRoute();
 		
 		mockMvc.perform(MockMvcRequestBuilders.get("/admin/route"))
 		.andExpect(status().isOk())
@@ -92,15 +111,14 @@ public class RouteApiTest {
 		.andExpect(content().json(json));
 	}
 	
+//	
+	
 	@Test
-	@DisplayName("Test Update route")
+	@DisplayName("Test Update Route")
 	public void testUpdateRoute() throws Exception{
 		
-		RouteModel newroute=new RouteModel(4, "Mumbai", "Bangalore", 836);
+		RouteModel newroute=new RouteModel(1,"Mumbai", "Delhi", 1163);
 		
-		doReturn(newroute).when(service).updateCity(ArgumentMatchers.any());
-		
-	    
 		 String json = new ObjectMapper().writeValueAsString(newroute);
 		
 		mockMvc.perform(put("/admin/route/update/")
@@ -112,8 +130,8 @@ public class RouteApiTest {
 				
 				.andExpect(status().isCreated())
 				.andExpect(content().json(json));
+				
 	}
-	
 	@Test
 	@DisplayName("Test Add list of route")
 	public void testAddAllRoute() throws Exception{
@@ -125,10 +143,7 @@ public class RouteApiTest {
 				List<RouteModel> routes = new ArrayList<>();
 				routes.add(route1);
 				routes.add(route2);
-				
-		
-		doReturn(routes).when(service).saveall(ArgumentMatchers.any());
-		
+						
 		String json = new ObjectMapper().writeValueAsString(routes);
 		
 		mockMvc.perform(post("/admin/route/bulk")
@@ -144,6 +159,5 @@ public class RouteApiTest {
 				.andExpect(content().json(json));
 
 	}
-	
 	
 }
