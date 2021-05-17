cc72da107b44a6fe24c1543681cf61c195977351
Deep <1607deep@gmail.com>
2021-05-18 04:13:41 +0530

    Before Merge updates

--- src/main/java/com/myapp/spring/web/api/UserAPI.java
+++ src/main/java/com/myapp/spring/web/api/UserAPI.java
@@ -1,4 +1,4 @@
-package com.myapp.spring.web;
+package com.myapp.spring.web.api;
 
 	
 import java.util.List;
--- src/test/java/com/myapp/spring/repsoitory/UserRepositoryTest.java
+++ src/test/java/com/myapp/spring/repsoitory/UserRepositoryTest.java
@@ -2,41 +2,73 @@ package com.myapp.spring.repsoitory;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.Arrays;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
+import org.springframework.test.web.servlet.MockMvc;
 
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.myapp.spring.model.User;
 import com.myapp.spring.repository.UserRepository;
  
-@DataJpaTest
-@AutoConfigureTestDatabase(replace = Replace.NONE)
-@Rollback(false)
+
+@SpringBootTest
 public class UserRepositoryTest {
  
-    @Autowired
-    private TestEntityManager entityManager;
+	@Autowired
+	private UserRepository repository;
+	
+	
+private static File DATA_JSON= Paths.get("src","test","resources","User.json").toFile();
+	
+	@BeforeEach
+	public void setup() throws JsonParseException, JsonMappingException, IOException {
+	
+	User users[]=new ObjectMapper().readValue(DATA_JSON,User[].class);
+	
+	//save each product to database
+	Arrays.stream(users).forEach(repository::save);
+	}
+
+@AfterEach
+public void cleanUp() {
+	repository.deleteAll();
+}
+
+@Test
+
+@DisplayName("Test user saved sucessfully")
+public void testProductSavedSucessfully() {
+	
+	User user =new User(1,"deep@mail.com","Vivo12pro","Deep","Shah");
+	
+	User savedUser =repository.save(user);
+	Assertions.assertNotNull(savedUser," New user should be saved");
+	
+	Assertions.assertNotNull(savedUser.getId()," New Product should have id");
+	
+	Assertions.assertEquals(user.getFirstName(), savedUser.getFirstName());
+	
+}
      
-    @Autowired
-    private UserRepository repo;
-     
-    @Test
-    public void testCreateUser() {
-        User user = new User();
-        user.setEmail("rajkumar@gmail.com");
-        user.setPassword("raj2021");
-        user.setFirstName("Raj");
-        user.setLastName("Kumar");
+
          
-        User savedUser = repo.save(user);
-         
-        User existUser = entityManager.find(User.class, savedUser.getId());
-         
-        assertThat(user.getEmail()).isEqualTo(existUser.getEmail());
-         
-    }
+    
 }
